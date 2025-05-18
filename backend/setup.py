d57aba58e09eb3e431ee692f574c2c70b2ef32a5
walidhbabou <161231266+walidhbabou@users.noreply.github.com>
2025-05-19 01:00:44 +0200

    update

---
+++ backend/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="backend",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        # Ajoutez vos dépendances ici, par exemple:
+        # "Flask==2.0.2",
+        # "Flask-Cors==3.0.10",
+        # etc.
+    ],
+) 
